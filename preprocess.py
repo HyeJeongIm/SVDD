4acc81f63252fe3d4df1fe8f60a4ab0d31e3f696
ImHyeJeong <dlaowjde@naver.com>
2024-04-30 14:46:38 +0900

    feat: only 2 classes dataloader

--- preprocess.py
+++ preprocess.py
@@ -10,6 +10,7 @@ from torchvision import datasets, transforms
 from PIL import Image
 
 from utils.utils import global_contrast_normalization
+from torch.utils.data import ConcatDataset
 
 
 class MNIST_loader(data.Dataset):
@@ -31,7 +32,14 @@ class MNIST_loader(data.Dataset):
     def __len__(self):
         """number of samples."""
         return len(self.data)
+    
+def get_mnist_combined_test_dataloader(dataloader_test_normal, dataloader_test_abnormal, batch_size):
+    """Combine two dataloaders into one."""
+    
+    combined_test_dataset = ConcatDataset([dataloader_test_normal.dataset, dataloader_test_abnormal.dataset])
+    dataloader_test_combined = DataLoader(combined_test_dataset, batch_size=batch_size, shuffle=False, num_workers=0)
 
+    return dataloader_test_combined
 
 def get_mnist(args, data_dir='./data/mnist/'):
     """get dataloders"""
@@ -46,29 +54,50 @@ def get_mnist(args, data_dir='./data/mnist/'):
                 (-0.7129780970522351, 12.057777597673047),
                 (-0.8280402650205075, 10.581538445782988),
                 (-0.7369959242164307, 10.697039838804978)]
-
+    
     transform = transforms.Compose([transforms.ToTensor(),
                                     transforms.Lambda(lambda x: global_contrast_normalization(x)),
                                     transforms.Normalize([min_max[args.normal_class][0]],
                                                          [min_max[args.normal_class][1] \
                                                          -min_max[args.normal_class][0]])])
-    train = datasets.MNIST(root=data_dir, train=True, download=True)
-    test = datasets.MNIST(root=data_dir, train=False, download=True)
-
-    x_train = train.data
-    y_train = train.targets
-
-    x_train = x_train[np.where(y_train==args.normal_class)]
-    y_train = y_train[np.where(y_train==args.normal_class)]
-                                    
-    data_train = MNIST_loader(x_train, y_train, transform)
-    dataloader_train = DataLoader(data_train, batch_size=args.batch_size, 
-                                  shuffle=True, num_workers=0)
     
-    x_test = test.data
-    y_test = test.targets
-    y_test = np.where(y_test==args.normal_class, 0, 1)
-    data_test = MNIST_loader(x_test, y_test, transform)
-    dataloader_test = DataLoader(data_test, batch_size=args.batch_size, 
-                                  shuffle=True, num_workers=0)
+    # 데이터 로딩 및 변환
+    all_train = datasets.MNIST(root=data_dir, train=True, download=True)
+    all_test = datasets.MNIST(root=data_dir, train=False, download=True)
+
+    '''
+        train dataset
+    '''
+
+    # 정상(normal) 클래스 필터링
+    normal_indices = all_train.targets == args.normal_class
+    x_train_normal = all_train.data[normal_indices]
+    y_train_normal = all_train.targets[normal_indices]
+
+    '''
+        test dataset
+    '''
+
+    # 정상(normal) 클래스 - 테스트 데이터
+    x_test_normal = all_test.data[all_test.targets == args.normal_class]
+    y_test_normal = all_test.targets[all_test.targets == args.normal_class]
+
+    # 비정상(abnormal) 클래스 필터링 - 테스트 데이터에서만
+    abnormal_indices = all_test.targets == args.abnormal_class
+    x_test_abnormal = all_test.data[abnormal_indices]
+    y_test_abnormal = all_test.targets[abnormal_indices]
+
+    # 데이터셋 생성
+    train_dataset = MNIST_loader(x_train_normal, y_train_normal, transform)
+    test_normal_dataset = MNIST_loader(x_test_normal, y_test_normal, transform)
+    test_abnormal_dataset = MNIST_loader(x_test_abnormal, y_test_abnormal, transform)
+
+    # 데이터로더 생성
+    dataloader_train = DataLoader(train_dataset, batch_size=args.batch_size, shuffle=True, num_workers=0)
+    dataloader_test_normal = DataLoader(test_normal_dataset, batch_size=args.batch_size, shuffle=False, num_workers=0)
+    dataloader_test_abnormal = DataLoader(test_abnormal_dataset, batch_size=args.batch_size, shuffle=False, num_workers=0)
+    
+    # combined test dataset
+    dataloader_test = get_mnist_combined_test_dataloader(dataloader_test_normal, dataloader_test_abnormal, args.batch_size)
+    
     return dataloader_train, dataloader_test
