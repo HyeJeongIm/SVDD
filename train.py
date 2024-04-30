38c2db493b70daea85e660ca13539606397bd46d
ImHyeJeong <dlaowjde@naver.com>
2024-04-30 13:03:34 +0900

    feat: add weights_path, pretrained_weight_path

--- train.py
+++ train.py
@@ -7,23 +7,34 @@ from barbar import Bar
 
 from model import autoencoder, network
 from utils.utils import weights_init_normal
-
+import os
 
 class TrainerDeepSVDD:
     def __init__(self, args, data, device):
         self.args = args
         self.train_loader, self.test_loader = data
         self.device = device
-    
+
+        # Create the directory path for the weights, including the parent directory.
+        self.weights_path = f'/home/cal-05/hj/SVDD/weights/mnist/{self.args.normal_class}'
+        self.pretrained_weights_path = f'{self.weights_path}/pretrained_parameters.pth'
+
+        # Ensure the directory for the weights exists.
+        self.ensure_directory_exists(self.weights_path)
+
+    def ensure_directory_exists(self, path):
+        if not os.path.exists(path):
+            os.makedirs(path, exist_ok=True)
+            print(f"Created directory: {path}")
+        else:
+            print(f"Directory already exists: {path}")
 
     def pretrain(self):
-        """ Pretraining the weights for the deep SVDD network using autoencoder"""
+        """Pretraining the weights for the deep SVDD network using autoencoder"""
         ae = autoencoder(self.args.latent_dim).to(self.device)
         ae.apply(weights_init_normal)
-        optimizer = optim.Adam(ae.parameters(), lr=self.args.lr_ae,
-                               weight_decay=self.args.weight_decay_ae)
-        scheduler = optim.lr_scheduler.MultiStepLR(optimizer, 
-                    milestones=self.args.lr_milestones, gamma=0.1)
+        optimizer = optim.Adam(ae.parameters(), lr=self.args.lr_ae, weight_decay=self.args.weight_decay_ae)
+        scheduler = optim.lr_scheduler.MultiStepLR(optimizer, milestones=self.args.lr_milestones, gamma=0.1)
         
         ae.train()
         for epoch in range(self.args.num_epochs_ae):
@@ -39,20 +50,16 @@ class TrainerDeepSVDD:
                 
                 total_loss += reconst_loss.item()
             scheduler.step()
-            print('Pretraining Autoencoder... Epoch: {}, Loss: {:.3f}'.format(
-                   epoch, total_loss/len(self.train_loader)))
+            print('Pretraining Autoencoder... Epoch: {}, Loss: {:.3f}'.format(epoch, total_loss/len(self.train_loader)))
         self.save_weights_for_DeepSVDD(ae, self.train_loader) 
     
-
     def save_weights_for_DeepSVDD(self, model, dataloader):
         """Initialize Deep SVDD weights using the encoder weights of the pretrained autoencoder."""
         c = self.set_c(model, dataloader)
         net = network(self.args.latent_dim).to(self.device)
         state_dict = model.state_dict()
         net.load_state_dict(state_dict, strict=False)
-        torch.save({'center': c.cpu().data.numpy().tolist(),
-                    'net_dict': net.state_dict()}, 'weights/pretrained_parameters.pth')
-    
+        torch.save({'center': c.cpu().data.numpy().tolist(), 'net_dict': net.state_dict()}, self.pretrained_weights_path)
 
     def set_c(self, model, dataloader, eps=0.1):
         """Initializing the center for the hypersphere"""
@@ -75,7 +82,7 @@ class TrainerDeepSVDD:
         net = network().to(self.device)
         
         if self.args.pretrain==True:
-            state_dict = torch.load('weights/pretrained_parameters.pth')
+            state_dict = torch.load(self.pretrained_weights_path)
             net.load_state_dict(state_dict['net_dict'])
             c = torch.Tensor(state_dict['center']).to(self.device)
         else:
