9fea694535fd95cf58970d45499e307aba8eca61
ImHyeJeong <dlaowjde@naver.com>
2024-05-01 20:12:46 +0900

    feat: 2 class final code for MNIST dataset

--- test.py
+++ test.py
@@ -1,23 +1,44 @@
+# import torch
+
+# def eval(net, c, dataloader, device):
+#     """Testing the Deep SVDD model"""
+
+#     scores = []
+#     labels = []
+#     net.eval()
+#     print('Testing...')
+#     with torch.no_grad():
+#         for x, y in dataloader:
+#             x = x.float().to(device)
+#             z = net(x)
+#             score = torch.sum((z - c) ** 2, dim=1)
+
+#             scores.append(score.detach().cpu())
+#             labels.append(y.cpu())
+#     labels, scores = torch.cat(labels).numpy(), torch.cat(scores).numpy()
+#     return labels, scores
+
+import matplotlib.pyplot as plt
+import numpy as np
 import torch
-
 from sklearn.metrics import roc_auc_score
-
-
+from tqdm import tqdm
 def eval(net, c, dataloader, device):
-    """Testing the Deep SVDD model"""
 
+    """Testing the Deep SVDD model, returning indices, labels, and scores for further analysis, and visualizing samples."""
     scores = []
     labels = []
+    indices = []
+    
     net.eval()
     print('Testing...')
     with torch.no_grad():
-        for x, y in dataloader:
+        for i, (x, y) in enumerate(tqdm(dataloader, desc='Testing')):
             x = x.float().to(device)
             z = net(x)
-            score = torch.sum((z - c) ** 2, dim=1)
+            dist = torch.sum((z - c) ** 2, dim=1)
+            scores.extend(dist.cpu().numpy())
+            labels.extend(y.cpu().numpy())
+            indices.extend(list(range(i * dataloader.batch_size, (i + 1) * dataloader.batch_size)))
 
-            scores.append(score.detach().cpu())
-            labels.append(y.cpu())
-    labels, scores = torch.cat(labels).numpy(), torch.cat(scores).numpy()
-    print('ROC AUC score: {:.2f}'.format(roc_auc_score(labels, scores)*100))
-    return labels, scores
+    return indices, labels, scores
