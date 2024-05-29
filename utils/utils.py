4edcf51dfff0d12a089b7ae708fc2be42aff9bd8
ImHyeJeong <dlaowjde@naver.com>
2024-05-29 21:45:29 +0900

    add comment

--- utils/utils.py
+++ utils/utils.py
@@ -11,8 +11,16 @@ def weights_init_normal(m):
 
 def global_contrast_normalization(x):
     """Apply global contrast normalization to tensor. """
+
+    # 이미지의 모든 pixel 값에 대한 평균을 계산
+    # 평균이 0이 되도록 함
+    # 이미지의 밝기 차이를 제거하여 대비에만 집중할 수 있게 함 
     mean = torch.mean(x)  # mean over all features (pixels) per sample
     x -= mean
+
+    # 전체 이미지 pixel이 평균으로부터 얼마나 떨어져 있는지를 나타내는 척도 
     x_scale = torch.mean(torch.abs(x))
+    # 데이터의 스케일을 조정하여 대비를 표준화하는 과정
     x /= x_scale
+
     return x
